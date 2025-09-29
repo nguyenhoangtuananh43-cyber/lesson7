dce87bb2e3eadcbd31b615b05de89a28ce659072
nguyenhoangtuananh43-cyber <nguyenhoangtuananh43@gmail.com>
2025-09-29 14:16:25 +0700

    Add files via upload

---
+++ luyentap2.js
@@ -0,0 +1,68 @@
+//bài 1
+//let a = [
+  //  [1, 2, 1, 24],
+    //[8, 11, 9, 4],
+    //[7, 0, 7, 27],
+    //[7, 4, 28, 14],
+    //[3, 10, 26, 7]
+//];
+
+//for (let i = 0; i < a.length; i++) {
+  //  console.log("row " + i);
+    //for (let j = 0; j < a[i].length; j++) {
+      //  console.log(" " + a[i][j]);
+    //}
+//}
+
+
+
+//bài 2
+//let arr = ['c', 's', 'c', '2', '6', '1'];
+//let result = arr.reverse().join("");
+//console.log(result);
+
+
+//bài 3
+//let arr = ['a', '1', 'b', '5', 'c', '9', 'x', '0'];
+//let count = 0;
+
+//for (let i = 0; i < arr.length; i++) {
+  //  if (!isNaN(arr[i]) && arr[i] !== " ") {
+    //    count++;
+ //   /}
+//}
+
+//console.log("Mảng:", arr);
+//console.log("Số lượng ký tự số (0-9):", count);
+
+
+
+
+// bài 4
+//let n ="Xin chào các bạn,mình đang học nè ";
+//let words = n.trim().split(/\s+/);
+//console.log("số từ trong chuỗi là" + words.length);
+
+
+
+//bài 5
+//let n = "hello word"
+//let t = "hello word"
+//if (n === t){
+  //  console.log("Hai chuỗi giống nhau")
+//}else {
+  //  console.log("Hai chuỗi khác nhau")
+//}
+
+
+
+//Bài 6
+//let arr = ['a', '-', 'b', '-', 'c', 'd', '-'];
+
+//for (let i = 0; i < arr.length; i++) {
+  //  if (arr[i] === '-') {
+    //    arr[i] = '_';
+    //}
+//}
+
+//console.log("Mảng sau khi thay thế:", arr);
